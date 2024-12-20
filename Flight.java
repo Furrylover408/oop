69a4ca4eb3207ba38c3e70e69c4da9bc882c442b
Furrylover408 <temirlanserik079@gmail.com>
2024-12-20 10:01:40 +0500

    Create Flight.java

---
+++ Flight.java
@@ -0,0 +1,37 @@
+// Flight.java
+public class Flight {
+    private String flightNumber;
+    private String airline;
+    private String origin;
+    private String destination;
+
+    public Flight(String flightNumber, String airline, String origin, String destination) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    // Getters and Setters
+    public String getFlightNumber() { return flightNumber; }
+    public void setFlightNumber(String flightNumber) { this.flightNumber = flightNumber; }
+
+    public String getAirline() { return airline; }
+    public void setAirline(String airline) { this.airline = airline; }
+
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "flightNumber='" + flightNumber + '\'' +
+                ", airline='" + airline + '\'' +
+                ", origin='" + origin + '\'' +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
+}
