2e630063bddcfa2a94855ab732877df0b0d9a093
Furrylover408 <temirlanserik079@gmail.com>
2025-01-10 10:43:15 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-// Main.java
 public class Main {
     public static void main(String[] args) {
+
         Flight flight1 = new Flight("AA123", "American Airlines", "New York", "Los Angeles");
         Flight flight2 = new Flight("BA456", "British Airways", "London", "New York");
 
@@ -10,10 +10,19 @@ public class Main {
         Booking booking1 = new Booking(flight1, passenger1, "BKG123");
         Booking booking2 = new Booking(flight2, passenger2, "BKG456");
 
-        System.out.println(booking1);
-        System.out.println(booking2);
+        BookingService bookingService = new BookingService();
+        bookingService.addBooking(booking1);
+        bookingService.addBooking(booking2);
 
-        // Comparing bookings
-        System.out.println("Are bookings equal? " + booking1.equals(booking2));
+        bookingService.displayBookings();
+
+        System.out.println("\nFiltered by airline 'British Airways':");
+        bookingService.filterBookingsByAirline("British Airways").forEach(System.out::println);
+
+        System.out.println("\nSearching for bookings by 'John Doe':");
+        bookingService.searchBookingsByPassengerName("John Doe").forEach(System.out::println);
+
+        System.out.println("\nBookings sorted by booking reference:");
+        bookingService.sortBookingsByReference().forEach(System.out::println);
     }
 }
